ad1b6b267716d5e405b21f85b6c0067d793375c1
wonka <guillermo.hurtado02@inacapmail.cl>
2025-07-13 00:24:35 -0400

    feat: Add SSL certificate and key files for secure server configuration

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,17 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
 import tailwindcss from '@tailwindcss/vite';
+import fs from 'fs';
+import path from 'path';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()]
+  plugins: [react(), tailwindcss()],
+  server: {
+    https: {
+      key: fs.readFileSync(path.resolve(__dirname, 'certs/cert.key')),
+      cert: fs.readFileSync(path.resolve(__dirname, 'certs/cert.crt')),
+    },
+    port: 5173,
+  }
 })
